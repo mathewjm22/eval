8dffc202c92080c19919cd442ab445108225080b
Jacob Mathew <75663388+mathewjm22@users.noreply.github.com>
2026-03-03 11:04:45 -0700

    Add JSDoc comment to cn function
    Added JSDoc comment for the cn function.

--- src/utils/cs.ts
+++ src/utils/cs.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Tailwind/clsx className helper (shadcn-style)
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
